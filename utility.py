b3813a84be5a5bf55b6f337e95ecb7f9353ac478
Melvin <melvin0719@hotmail.com>
2017-07-10 17:33:01 +0800

    Add new distance, bearing functions for x, y coords and refactor latlng distance bearing functions

--- utility.py
+++ utility.py
@@ -19,56 +19,64 @@ split_list = lambda list_index: [list_index[0][:list_index[1]], list_index[0][li
 # Takes a list and a value; Returns the first index of the list that is greater than the value. 
 find_first_index_greater_than = lambda list_value: next((index for index, value in enumerate(list_value[0]) if value > list_value[1]), None)
 
+# Compute distance given two coordinates in (x, y) form
+def xy_distance(xy1, xy2):
+    diff_x = xy1[0] - xy2[0]
+    diff_y = xy1[1] - xy2[1]
+    return math.sqrt(diff_x * diff_x + diff_y * diff_y)
+
+# Compute bearing from first point to second point in (x, y) form
+def xy_bearing(xy1, xy2):
+    theta = math.atan2(xy2[0] - xy1[0], xy2[1] - xy1[1])
+    if theta < 0.0:
+        theta += 2 * math.pi
+    return theta * 180 / math.pi
+
 # Compute distance given two lat lngs
-def distance(lat1, lng1, lat2, lng2):
-    rr_1 = [lat1 / 180 * math.pi, lng1 / 180 * math.pi]
-    rr_2 = [lat2 / 180 * math.pi, lng2 / 180 * math.pi]
+def latlng_distance(ll1, ll2):
+    rr_1 = [ll1[0] / 180 * math.pi, ll1[1] / 180 * math.pi]
+    rr_2 = [ll2[0] / 180 * math.pi, ll2[1] / 180 * math.pi]
     dx = (rr_1[1] - rr_2[1]) * math.cos(0.5 * (rr_1[0] + rr_2[0]))
     dy = rr_1[0] - rr_2[0]
     dist = math.sqrt(dx * dx + dy * dy) * 6371000
     return dist
 
 # Compute bearing between two lat lngs (in degrees from 0 to 359)
-def bearing(lat1, lng1, lat2, lng2):
-    # Code taken from https://gist.github.com/jeromer/2005586
-    def calculate_initial_compass_bearing(pointA, pointB):
-        """
-        Calculates the bearing between two points.
-        The formulae used is the following:
-            θ = atan2(sin(Δlong).cos(lat2),
-                      cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
-        :Parameters:
-          - `pointA: The tuple representing the latitude/longitude for the
-            first point. Latitude and longitude must be in decimal degrees
-          - `pointB: The tuple representing the latitude/longitude for the
-            second point. Latitude and longitude must be in decimal degrees
-        :Returns:
-          The bearing in degrees
-        :Returns Type:
-          float
-        """
-        if (type(pointA) != tuple) or (type(pointB) != tuple):
-            raise TypeError("Only tuples are supported as arguments")
+# Code taken from https://gist.github.com/jeromer/2005586
+def latlng_bearing(pointA, pointB):
+    """
+    Calculates the bearing between two points.
+    The formulae used is the following:
+        θ = atan2(sin(Δlong).cos(lat2),
+                  cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
+    :Parameters:
+      - `pointA: The tuple representing the latitude/longitude for the
+        first point. Latitude and longitude must be in decimal degrees
+      - `pointB: The tuple representing the latitude/longitude for the
+        second point. Latitude and longitude must be in decimal degrees
+    :Returns:
+      The bearing in degrees
+    :Returns Type:
+      float
+    """
+    lat1 = math.radians(pointA[0])
+    lat2 = math.radians(pointB[0])
 
-        lat1 = math.radians(pointA[0])
-        lat2 = math.radians(pointB[0])
+    diffLong = math.radians(pointB[1] - pointA[1])
 
-        diffLong = math.radians(pointB[1] - pointA[1])
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
+            * math.cos(lat2) * math.cos(diffLong))
 
-        x = math.sin(diffLong) * math.cos(lat2)
-        y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1)
-                * math.cos(lat2) * math.cos(diffLong))
+    initial_bearing = math.atan2(x, y)
 
-        initial_bearing = math.atan2(x, y)
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180° to + 180° which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
 
-        # Now we have the initial bearing but math.atan2 return values
-        # from -180° to + 180° which is not what we want for a compass bearing
-        # The solution is to normalize the initial bearing as shown below
-        initial_bearing = math.degrees(initial_bearing)
-        compass_bearing = (initial_bearing + 360) % 360
-
-        return compass_bearing
-    return calculate_initial_compass_bearing((lat1, lng1), (lat2, lng2))
+    return compass_bearing
 
 # Obtain the value in the n-th percentile. (Where n is in the range [0, 1])
 def percentile(values, n):
