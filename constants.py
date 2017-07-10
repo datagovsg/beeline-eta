f6968ff466e7a93fea576b9a7ae94207f7912d82
Melvin <melvin0719@hotmail.com>
2017-07-10 16:38:56 +0800

    Multiprocess prediction for each trip, and cache results in separate files

--- constants.py
+++ constants.py
@@ -2,5 +2,4 @@ API_KEY = 'AIzaSyCxvr7c1N-0YuzYkEl9k9Z3n9RCA9tEL4c'
 DATE_FORMAT = '%Y-%m-%d'
 TIME_FORMAT = '%H:%M:%S'
 DATETIME_FORMAT = ' '.join([DATE_FORMAT, TIME_FORMAT])
-API_RESULT_FILE = 'results/api-result.pickle'
 MAX_TIME = 900 # Not predicting anything over 10 mins (we declare as >10 mins)
--- run.py
+++ run.py
@@ -1,26 +1,13 @@
-import threading
-from constants import API_RESULT_FILE
+import multiprocessing
 from datetime import datetime
-from save_and_load_variables import write_to_pickle
 from trip_helper import get_operating_trip_ids
 from trip_predictor import update_timings_for_trip
 
 def run(date_time):
-    api_result = {}
-    
     operating_trip_ids = get_operating_trip_ids(date_time)
 
-    threads = []
     for trip_id in operating_trip_ids:
-        # Do multithread for each trip to update timings.
-        t = threading.Thread(target=update_timings_for_trip,
-                             args=(api_result, date_time, trip_id))
-        threads.append(t)
-    for t in threads: # Start all threads
-        t.start()
-    for t in threads: # Wait for all threads to finish
-        t.join()
-    
-    # Rewrite api-result.pickle
-    write_to_pickle(API_RESULT_FILE, api_result)
-    return api_result
+        # Do an independent process for each trip to update timings.
+        p = multiprocessing.Process(target=update_timings_for_trip,
+                                    args=(date_time, trip_id))
+        p.start()
